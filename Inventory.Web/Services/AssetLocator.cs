960cbf317c7ee941938aa44aee12e44653f22cee
Shaun T Martin <webmaster@ginblades.com>
2019-06-21 19:36:15 -0500

    Setup asset locator for working with manifest.

---
+++ Inventory.Web/Services/AssetLocator.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Web.Services
+{
+    public class AssetLocator
+    {
+        private readonly IHostingEnvironment _env;
+
+        public AssetLocator(IHostingEnvironment env)
+        {
+            _env = env;
+        }
+
+        public string GetAsset(string keyName)
+        {
+            using (StreamReader reader = new StreamReader($"{_env.WebRootPath}/build/manifest.json"))
+            {
+                var json = reader.ReadToEnd();
+                var mapping = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+                return mapping[keyName];
+            }
+        }
+    }
+}
--- Inventory.Web/Startup.cs
+++ Inventory.Web/Startup.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Inventory.Web.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -30,6 +31,7 @@ namespace Inventory.Web
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
+            services.AddSingleton<AssetLocator>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
